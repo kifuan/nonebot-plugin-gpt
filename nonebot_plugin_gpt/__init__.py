9468428aa1ae5ba14f3cca6781055269ef75b8d6
kifuan <kifuan@foxmail.com>
2022-12-06 16:03:06 +0800

    feat: check permission and pass group id

--- nonebot_plugin_gpt/__init__.py
+++ nonebot_plugin_gpt/__init__.py
@@ -1,33 +1,38 @@
-from nonebot import on_command, Bot
-from nonebot.matcher import Matcher
+from nonebot import on_command
 from nonebot.adapters.onebot.v11.event import GroupMessageEvent
 from .chatbot import Chatbot
+from .config import gpt_config
+
 
 chat = on_command('chat')
-
-gpt = on_command('gpt')
+gpt = on_command('chat_control')
 
 
 @chat.handle()
-async def _(_: Bot, __: Matcher, event: GroupMessageEvent):
+async def _(event: GroupMessageEvent):
     cb = await Chatbot.get_instance()
     text = event.get_message().extract_plain_text()
-    async for line in cb.get_chat_lines(text):
+    async for line in cb.get_chat_lines(event.group_id, text):
         await chat.send(line)
 
 
 @gpt.handle()
-async def _(_: Bot, __: Matcher, event: GroupMessageEvent):
+async def _(event: GroupMessageEvent):
+    if event.sender.user_id not in gpt_config.gpt_sudoers:
+        await gpt.send('没有权限')
+        return
+
     text = event.get_message().extract_plain_text()
     cb = await Chatbot.get_instance()
 
     if text == 'refresh_session':
         await cb.refresh_session()
         await gpt.send('刷新成功')
+        return
 
-    elif text == 'reset_status':
+    if text == 'reset_status':
         cb.reset_or_create_status(event.group_id)
         await gpt.send('重置成功')
+        return
 
-    else:
-        await gpt.send('无效命令')
+    await gpt.send('无效命令')
