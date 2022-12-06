c2114924bacb54e652770ee314a0000d9145c31a
kifuan <kifuan@foxmail.com>
2022-12-06 12:56:20 +0800

    refactor: use stream output instead of text output

--- nonebot_plugin_gpt/__init__.py
+++ nonebot_plugin_gpt/__init__.py
@@ -1,8 +1,7 @@
 from nonebot import on_command, Bot
 from nonebot.matcher import Matcher
 from nonebot.adapters.onebot.v11.event import GroupMessageEvent
-from nonebot.log import logger
-from .chatbot import get_chatbot
+from .chatbot import get_response_lines
 
 chat = on_command('chat')
 
@@ -12,8 +11,5 @@ gpt = on_command('gpt')
 @chat.handle()
 async def _(_: Bot, __: Matcher, event: GroupMessageEvent):
     text = event.get_message().extract_plain_text()
-    logger.trace('group_id', event.group_id)
-    logger.trace('text', text)
-    cb = get_chatbot()
-    resp = cb.get_chat_response(text, output='text')
-    await chat.send(resp['message'])
+    for line in get_response_lines(text):
+        await chat.send(line)
--- nonebot_plugin_gpt/chatbot.py
+++ nonebot_plugin_gpt/chatbot.py
@@ -1,5 +1,6 @@
-from functools import lru_cache
 from revChatGPT.revChatGPT import Chatbot
+from functools import lru_cache
+from typing import Generator
 
 from .config import gpt_config
 
@@ -15,3 +16,18 @@ def get_chatbot() -> Chatbot:
     cb.refresh_session()
 
     return cb
+
+
+def get_response_lines(text: str) -> Generator[str, None, None]:
+    chatbot = get_chatbot()
+    line = ''
+    skip = 0
+    for resp in chatbot.get_chat_response(text, output='stream'):
+        line = resp['message'][skip:]
+        if '\n' in line:
+            skip += len(line)
+            yield line.strip()
+
+    # Check the line here to avoid missing the last line.
+    if line != '':
+        yield line.strip()
